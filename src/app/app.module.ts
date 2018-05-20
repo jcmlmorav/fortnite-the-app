4e84a612a4507aa032154086c88525b59d4ab90b
Juan Camilo Mora <jcmora@smdigital.com.co>
2018-05-20 15:39:54 -0500

    Accordion implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AccordionModule } from 'ngx-accordion';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AccordionModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
